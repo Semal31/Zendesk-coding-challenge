5c52d56dcd32b7fe2ed053267da73ac0a1056523
Semal Shah <semal31@outlook.com>
2021-11-28 00:56:08 -0600

    📦  FEAT: [test_CLI] add in all the tests [helper] add some error handling and format better

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import textwrap
+import sys
 from tabulate import tabulate
 
 def getInput(prompt, constraint):
@@ -20,20 +21,33 @@ def getInput(prompt, constraint):
 
 def printTickets(tickets, allTicketsView):
   formattedTickets = {}
+  error = False
   if allTicketsView:
     for ticket in tickets:
-      formattedTickets['ID'] = formattedTickets.get('ID', []) + [ticket['id']]
-      formattedTickets['Created At'] = formattedTickets.get('Created At', []) + [ticket['created_at']]
-      formattedTickets['Updated At'] = formattedTickets.get('Updated At', []) + [ticket['updated_at']]
-      formattedTickets['Priority'] = formattedTickets.get('Priority', []) + [ticket['priority']]
-      formattedTickets['Subject'] = formattedTickets.get('Subject', []) + [ticket['subject']]
+      try:
+        formattedTickets['ID'] = formattedTickets.get('ID', []) + [ticket['id']]
+        formattedTickets['Created At'] = formattedTickets.get('Created At', []) + [ticket['created_at']]
+        formattedTickets['Updated At'] = formattedTickets.get('Updated At', []) + [ticket['updated_at']]
+        formattedTickets['Priority'] = formattedTickets.get('Priority', []) + [ticket['priority']]
+        formattedTickets['Status'] = formattedTickets.get('Status', []) + [ticket['status']]
+        formattedTickets['Subject'] = formattedTickets.get('Subject', []) + [ticket['subject']]
+      except KeyError:
+        error = True
+        print("****PROGRAM ERROR: Correct formatted data not received from API call****")
+        break
   else:
-    formattedTickets['ID'] = [tickets[0]['id']]
-    formattedTickets['Created At'] = [tickets[0]['created_at']]
-    formattedTickets['Updated At'] = [tickets[0]['updated_at']]
-    formattedTickets['Type'] = [tickets[0]['type']]
-    formattedTickets['Priority'] = [tickets[0]['priority']]
-    formattedTickets['Subject'] = [tickets[0]['subject']]
-    formattedTickets['Description'] = ['\n'.join(textwrap.wrap(tickets[0]['description'], width=30))]
-
-  print(tabulate(formattedTickets, headers='keys', missingval='N/A'))
+    try:
+      formattedTickets['ID'] = [tickets[0]['id']]
+      formattedTickets['Created At'] = [tickets[0]['created_at']]
+      formattedTickets['Updated At'] = [tickets[0]['updated_at']]
+      formattedTickets['Type'] = [tickets[0]['type']]
+      formattedTickets['Priority'] = [tickets[0]['priority']]
+      formattedTickets['Status'] = [tickets[0]['status']]
+      formattedTickets['Subject'] = [tickets[0]['subject']]
+      formattedTickets['Description'] = ['\n'.join(textwrap.wrap(tickets[0]['description'], width=30))]
+    except KeyError:
+      error = True
+      print("****PROGRAM ERROR: Correct formatted data not received from API call****")
+      
+  if not error:
+    print(tabulate(formattedTickets, headers='keys', missingval='N/A'))
--- test_CLI.py
+++ test_CLI.py
@@ -1,5 +1,5 @@
 import pytest
-from helper import getInput
+from helper import getInput, printTickets
 
 # wanted to put all helper tests in their own class but setattr would not work in the class
 def test_getInput_charArray_correct(monkeypatch):
@@ -17,3 +17,108 @@ def test_getInput_char_incorrect(monkeypatch):
 def test_getInput_charArray_incorrect(monkeypatch):
   monkeypatch.setattr('builtins.input', lambda: "a")
   assert getInput("example prompt", ['a', 'b']) != 'c'
+
+def test_printTickets_allTicketsView_correct(capsys):
+  tickets = [
+    {
+      "id": 1,
+      "created_at": "2021-11-26T18:24:43Z",
+      "updated_at": "2021-11-26T18:24:43Z",
+      "type": "incident",
+      "subject": "Sample ticket: Meet the ticket",
+      "priority": "normal",
+      "status": "open"
+    },
+    {
+      "id": 2,
+      "created_at": "2021-11-26T18:41:26Z",
+      "updated_at": "2021-11-26T18:41:26Z",
+      "type": None,
+      "subject": "velit eiusmod reprehenderit officia cupidatat",
+      "priority": None,
+      "status": "open"
+    },
+    {
+      "id": 3,
+      "created_at": "2021-11-26T18:41:26Z",
+      "updated_at": "2021-11-26T18:41:26Z",
+      "type": None,
+      "subject": "excepteur laborum ex occaecat Lorem",
+      "priority": None,
+      "status": "open"
+    }
+  ]
+  textFile = open("Txt_Test_Files/allTicketsView_test.txt", "r")
+  expected = textFile.read()
+  printTickets(tickets, True)
+  captured = capsys.readouterr()
+  assert captured.out == expected
+
+def test_printTickets_indivTicketsView_correct(capsys):
+  ticket = [
+    {
+      "id": 2,
+      "created_at": "2021-11-26T18:41:26Z",
+      "updated_at": "2021-11-26T18:41:26Z",
+      "type": None,
+      "subject": "velit eiusmod reprehenderit officia cupidatat",
+      "description": "Aute ex sunt culpa ex ea esse sint cupidatat aliqua ex consequat sit reprehenderit. Velit labore proident quis culpa ad duis adipisicing laboris voluptate velit incididunt minim consequat nulla. Laboris adipisicing reprehenderit minim tempor officia ullamco occaecat ut laborum.\n\nAliquip velit adipisicing exercitation irure aliqua qui. Commodo eu laborum cillum nostrud eu. Mollit duis qui non ea deserunt est est et officia ut excepteur Lorem pariatur deserunt.",
+      "priority": None,
+      "status": "open"
+    }
+  ]
+  textFile = open("Txt_Test_Files/indivTicketView_test.txt", "r")
+  expected = textFile.read()
+  printTickets(ticket, False)
+  captured = capsys.readouterr()
+  assert captured.out == expected
+
+def test_printTickets_indivTicketsView_incorrect(capsys):
+  # missing the status field
+  ticket = [
+    {
+      "id": 2,
+      "created_at": "2021-11-26T18:41:26Z",
+      "updated_at": "2021-11-26T18:41:26Z",
+      "type": None,
+      "subject": "velit eiusmod reprehenderit officia cupidatat",
+      "description": "Aute ex sunt culpa ex ea esse sint cupidatat aliqua ex consequat sit reprehenderit. Velit labore proident quis culpa ad duis adipisicing laboris voluptate velit incididunt minim consequat nulla. Laboris adipisicing reprehenderit minim tempor officia ullamco occaecat ut laborum.\n\nAliquip velit adipisicing exercitation irure aliqua qui. Commodo eu laborum cillum nostrud eu. Mollit duis qui non ea deserunt est est et officia ut excepteur Lorem pariatur deserunt.",
+      "priority": None
+    }
+  ]
+  printTickets(ticket, False)
+  captured = capsys.readouterr()
+  assert captured.out == "****PROGRAM ERROR: Correct formatted data not received from API call****\n"
+
+def test_printTickets_allTicketsView_incorrect(capsys):
+  # missing the updated_at field in ticket 2
+  tickets = [
+    {
+      "id": 1,
+      "created_at": "2021-11-26T18:24:43Z",
+      "updated_at": "2021-11-26T18:24:43Z",
+      "type": "incident",
+      "subject": "Sample ticket: Meet the ticket",
+      "priority": "normal",
+      "status": "open"
+    },
+    {
+      "id": 2,
+      "created_at": "2021-11-26T18:41:26Z",
+      "type": None,
+      "subject": "velit eiusmod reprehenderit officia cupidatat",
+      "priority": None,
+      "status": "open"
+    },
+    {
+      "id": 3,
+      "created_at": "2021-11-26T18:41:26Z",
+      "updated_at": "2021-11-26T18:41:26Z",
+      "type": None,
+      "subject": "excepteur laborum ex occaecat Lorem",
+      "priority": None
+    }
+  ]
+  printTickets(tickets, True)
+  captured = capsys.readouterr()
+  assert captured.out == "****PROGRAM ERROR: Correct formatted data not received from API call****\n"
